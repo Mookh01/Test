112848dae6a49e97d853c165a11739b434742ca2
Keith Moore <k1a2m@hotmail.com>
2016-06-06 12:30:17 -0500

    "final push for project"

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 'use strict';
 
-var express    = require('express');        
-var app        = express();                
-var routes = require('./index.js');
-    
-var port = process.env.PORT || 8080;        
-    
-    
-routes(app);
+var express    = require('express');       //basic beginnings 
+var app        = express();              //basic beginnings  
+var routes = require('./index.js');    //connecting the files
+var port = process.env.PORT || 8080;    //basic beginnings         
+routes(app);  //passing express() into index.js
 
 app.listen(port, function() {
-    console.log('Node.js listening on port ' + port);
+    console.log('listening on port ' + port);
 });
